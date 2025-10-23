6b9cb314cf1ed28f99b7f6497acca4063c5efe01
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2025-10-23 16:12:00 -0300

    Interpolação de variáveis

---
+++ src/ruby-basico/05_interpolacao.rb
@@ -0,0 +1,13 @@
+nome = "Nicolas"
+sobrenome = "Yoshizawa"
+
+# Utilizando a adição de strings
+puts "Meu nome é " + nome + " " + sobrenome
+
+# Utilizando a interpolação de strings
+puts "Meu nome é #{nome} #{sobrenome}"
+
+# Exemplos de interpolação com expressões
+puts "2 + 2 = #{2 + 2}"
+
+# OBS: A interpolação de strings só funciona com aspas duplas (" ").
