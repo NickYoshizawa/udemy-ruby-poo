1a21ad57e9bade6d6be7aaee8cd9d6ec91f91e17
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2025-10-23 15:15:01 -0300

    Comentários

---
+++ src/ruby-basico/04_comentarios.rb
@@ -0,0 +1,22 @@
+# Isso é um comentário de uma única linha em Ruby
+
+
+=begin
+Isso é um comentário
+de múltiplas linhas
+em Ruby
+=end
+
+# exemplos:
+
+puts "Qual o seu nome?"
+nome = gets.chomp  # Lê a entrada do usuário sem a quebra de linha
+puts "Seu nome é " + nome
+
+
+=begin
+puts "Qual o seu nome?"
+nome = gets.chomp
+puts "Seu nome é " + nome
+=end
+
