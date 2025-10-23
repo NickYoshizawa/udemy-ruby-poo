c332161677d1d58a59180f96805be769c9e80b66
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2025-10-23 14:45:19 -0300

    Entradas e Saídas - continuação

--- src/ruby-basico/03_entrada_saida.rb
+++ src/ruby-basico/03_entrada_saida.rb
@@ -1,3 +1,10 @@
+# Com quebra de linha
 puts "Qual o seu nome?"
 nome = gets
+puts "Seu nome é " + nome
+
+
+# Sem quebra de linha
+puts "Qual o seu nome?"
+nome = gets.chomp
 puts "Seu nome é " + nome
