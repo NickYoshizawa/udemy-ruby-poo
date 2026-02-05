18089bf51bd5b407f0ae7a3735525761b349de3f
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2026-02-05 11:34:38 -0300

    Estruturas de Repeticao

---
+++ src/ruby-basico/12_estruturas_repeticao.rb
@@ -0,0 +1,13 @@
+i = 1
+
+while i <= 10 # Enquanto for verdadeiro
+    puts "(WHILE) Nicolas - #{i}"
+    i += 1
+end
+
+puts "=="*5
+
+until i <= 1 # Enquanto for falso
+    i -= 1
+    puts "(UNTIL) Nicolas - #{i}"
+end
