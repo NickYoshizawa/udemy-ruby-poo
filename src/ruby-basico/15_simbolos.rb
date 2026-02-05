78939f38bab3133befba658d2578d98c8fd725e1
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2026-02-05 12:05:30 -0300

    Simbolos

---
+++ src/ruby-basico/15_simbolos.rb
@@ -0,0 +1,15 @@
+# Simbolos são objetos imutáveis e únicos, frequentemente usados como identificadores ou chaves em hashes.
+
+h1 = {
+    :nome => "Maria", 
+    :idade => 25, 
+    :cidade => "Rio de Janeiro"
+}
+
+# ou 
+
+h2 = {
+    nome: "Carlos", 
+    idade: 30, 
+    cidade: "Belo Horizonte"
+}
