ea2f345411371fcd6d0f8a29302e316b9d131e05
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2026-02-05 10:26:06 -0300

    Operadores Aritimeticos

---
+++ src/ruby-basico/07_operadores_aritimeticos.rb
@@ -0,0 +1,18 @@
+# Soma
+1 + 1 # Resultado é 2
+
+# Subtração
+5 - 3 # Resultado é 2
+
+# Multiplicação
+4 * 2 # Resultado é 8
+
+# Divisão
+4 / 2 # Resultado é 2
+
+
+# Módulo -> Resto da divisão
+10 % 3 # Resto é 1
+
+# Potenciação
+2 ** 3 # Resultado é 8 (2 elevado a 3)
