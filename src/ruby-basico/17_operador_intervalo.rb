dd3777e43099cef6b8c72d7fb84f36d157f0e4e0
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2026-02-05 12:11:46 -0300

    Operador de Intervalo

---
+++ src/ruby-basico/17_operador_intervalo.rb
@@ -0,0 +1,9 @@
+# Operador (...) - Cria um intervalo que exclui o valor final
+puts "\n===== Operador (1...5) ====="
+puts (1...5).to_a  # Resultado: 1, 2, 3, 4
+
+
+
+# Operador (..) - Cria um intervalo que inclui o valor final
+puts "\n===== Operador (1..5) ====="
+puts (1..5).to_a   # Resultado: 1, 2, 3, 4, 5
