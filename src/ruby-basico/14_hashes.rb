cb40eca07154785504ea93f8499db1dbdb3c146f
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2026-02-05 11:50:09 -0300

    Hashes

---
+++ src/ruby-basico/14_hashes.rb
@@ -0,0 +1,11 @@
+# Hash -> {"key" => "value"}
+
+h = {
+    "nome" => "João", 
+    "idade" => 20, 
+    "cidade" => "São Paulo"
+}
+
+puts h["nome"]      # João
+puts h["idade"]     # 20
+puts h["cidade"]    # São Paulo   
