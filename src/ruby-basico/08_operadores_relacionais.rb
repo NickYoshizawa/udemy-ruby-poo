8f0d0eb0e3b321f48dd6bc0575c8ce4ee73be549
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2026-02-05 10:53:01 -0300

    Operadores Relacionais

---
+++ src/ruby-basico/08_operadores_relacionais.rb
@@ -0,0 +1,26 @@
+# É maior?
+1 > 2 # falso
+
+# É menor?
+1 < 2 # verdadeiro
+
+# É maior ou igual?
+1 >= 1 # verdadeiro
+
+# É menor ou igual?
+1 <= 2 # verdadeiro
+
+# É igual?
+1 == 1 # verdadeiro
+1 == 2 # falso
+
+# É diferente?
+1 != 2 # verdadeiro
+1 != 1 # falso
+
+# SpaceShip Operator (Nave espacial)
+# Retorna -1 se o valor da esquerda for menor, 0 se forem iguais e 1 se o valor da esquerda for maior
+1 <=> 2 # Retorna -1
+2 <=> 2 # Retorna 0
+3 <=> 2 # Retorna 1
+
